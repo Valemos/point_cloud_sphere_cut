c9811ffb0374386a26523485330bebe05b1ee9ca
Anton Skrypnyk <programworkerbox@gmail.com>
2022-04-10 00:49:34 +0300

    Add iteration test.

--- test_iterations.cpp
+++ test_iterations.cpp
@@ -1,7 +1,19 @@
 
 
+#include <iostream>
+#include "VolumeGridScanner.hpp"
 
 int main() {
+    cadcam::VolumeGridScanner<size_t> indexScanner{
+        {0, 0, 0},
+        {2, 2, 2},
+        1
+    };
+
+    while (indexScanner.InProgress()) {
+        auto index = indexScanner.NextPoint();
+        std::cout << index.x() << " " << index.y() << " " << index.z() << std::endl;
+    }
 
     return 0;
 }
